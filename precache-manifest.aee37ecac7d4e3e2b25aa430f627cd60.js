8a2312fe4a7089649e2142459af54b3919472101
dmarques <daniloymsguitar@gmail.com>
2020-05-25 23:01:46 +0900

    Updates

---
+++ precache-manifest.aee37ecac7d4e3e2b25aa430f627cd60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53385e2599eb19eaa5bd77bb7ed83b80",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "19965ef6b945c2c48096",
+    "url": "/me/static/css/main.f5de08f0.chunk.css"
+  },
+  {
+    "revision": "1254522c79a5a73fe12f",
+    "url": "/me/static/js/2.2006df43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.2006df43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19965ef6b945c2c48096",
+    "url": "/me/static/js/main.082d0218.chunk.js"
+  },
+  {
+    "revision": "117b1daf9eef0a5ed5a6",
+    "url": "/me/static/js/runtime-main.833d48b3.js"
+  }
+]);
